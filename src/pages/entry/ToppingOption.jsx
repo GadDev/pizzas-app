562a124f0b020fa85904b1b0e63f804febf8d160
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-15 22:19:07 +0000

    topping subtotal test and implementation

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 
-const ToppingOption = ({ name, image }) => {
+const ToppingOption = ({ name, image, updateItemCount }) => {
 	return (
 		<Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
 			<img
@@ -9,6 +10,15 @@ const ToppingOption = ({ name, image }) => {
 				src={`http://localhost:3030/${image}`}
 				alt={`${name} topping`}
 			/>
+			<Form.Group controlId={name}>
+				<Form.Check
+					type='checkbox'
+					label={name}
+					onChange={(e) =>
+						updateItemCount(name, e.target.checked ? 1 : 0)
+					}
+				/>
+			</Form.Group>
 		</Col>
 	);
 };
--- src/pages/entry/tests/totalUpdates.test.jsx
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,11 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../../test-utils/testing-library-utils';
 import userEvent from '@testing-library/user-event';
 
 import Options from '../Options';
 
 import { OrderDetailsProvider } from '../../../contexts/OrderDetails';
 test('update pizza subtotal when pizza change', async () => {
-	render(<Options optionType='pizzas' />, { wrapper: OrderDetailsProvider });
+	render(<Options optionType='pizzas' />);
 
 	// make sure toatl starts out 0
 	const pizzaSubTotal = screen.getByText('Pizzas total: $', { exact: false });
@@ -25,4 +25,36 @@ test('update pizza subtotal when pizza change', async () => {
 	userEvent.clear(fiorentinaInput);
 	userEvent.type(fiorentinaInput, '2');
 	expect(pizzaSubTotal).toHaveTextContent('30.00');
+});
+
+test('update toppings subtotal when toppings added', async () => {
+	render(<Options optionType='toppings' />);
+	const toppingsSubTotal = screen.getByText('Toppings total: $', {
+		exact: false,
+	});
+	expect(toppingsSubTotal).toHaveTextContent('0.00');
+	const MushroomCheckbox = await screen.findByRole('checkbox', {
+		name: 'Mushrooms',
+	});
+	const OliveCheckbox = await screen.findByRole('checkbox', {
+		name: 'Olive',
+	});
+
+	const BasilicCheckbox = await screen.findByRole('checkbox', {
+		name: 'Basilic',
+	});
+	expect(MushroomCheckbox).not.toBeChecked();
+
+	userEvent.click(MushroomCheckbox);
+	expect(toppingsSubTotal).toHaveTextContent('2.50');
+
+	userEvent.click(OliveCheckbox);
+	expect(toppingsSubTotal).toHaveTextContent('5.00');
+
+	userEvent.click(BasilicCheckbox);
+	expect(toppingsSubTotal).toHaveTextContent('7.50');
+
+	userEvent.click(MushroomCheckbox);
+	expect(MushroomCheckbox).not.toBeChecked();
+	expect(toppingsSubTotal).toHaveTextContent('5.00');
 });
