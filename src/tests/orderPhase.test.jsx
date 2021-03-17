29b6f62d353592436caebad884fe8174edcc2119
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-17 00:19:32 +0000

    wip

---
+++ src/tests/orderPhase.test.jsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App'
+
+test('order phases for happy path', () => {
+	// render app
+
+	// add pizza and toppings
+
+	// find and click order button
+
+	// check summary information based on order
+
+	// accept terms and conditions and click btn to confirm order
+
+	// confirm order number on confirmation page
+
+	// click 'new order' button confirmation page
+
+	// check that pizzas and toppings subtotals have been reset
+
+	// do we need to await anything to avoid test errors?
+})
