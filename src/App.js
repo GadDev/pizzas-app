8d1e8ce5f0b2fb507b58fe73c0ee628f5cdd7e48
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-22 18:46:17 +0000

    set switch phase

--- src/App.js
+++ src/App.js
@@ -7,14 +7,28 @@ import './App.css';
 import Navigation from './components/Navbar';
 import OrderEntry from './pages/entry/OrderEntry';
 import OrderSummary from './pages/summary/OrderSummary';
+import OrderConfirmation from './pages/confirmation/OrderConfirmation';
 
 import { OrderDetailsProvider } from './contexts/OrderDetails.jsx';
 function App() {
 	// order "inProgress", 'review', or 'completed'
-	const [phase, setPhase] = useState(null)
+	const [phase, setPhase] = useState(null);
 
 	//set default view
-	let Component = OrderEntry
+	let Component = OrderEntry;
+
+	switch (phase) {
+		case 'inProgress':
+			Component = OrderEntry;
+			break;
+		case 'review':
+			Component = OrderSummary;
+			break;
+		case 'completed':
+			Component = OrderConfirmation;
+			break;
+		default:
+	}
 	return (
 		<Router>
 			<Navigation />
