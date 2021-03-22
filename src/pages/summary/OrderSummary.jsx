e342b303fc0efabde8ba99bd98a9398fc43600ed
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-22 19:03:59 +0000

    get pizzas order detial

--- src/pages/summary/OrderSummary.jsx
+++ src/pages/summary/OrderSummary.jsx
@@ -1,8 +1,19 @@
-import SummaryForm from './SummaryForm'
-import { useOrderDetails } from '../../contexts/OrderDetails'
+import SummaryForm from './SummaryForm';
+import { useOrderDetails } from '../../contexts/OrderDetails';
 
 const OrderSummary = ({ setPhase }) => {
-	const [orderDetails] = useOrderDetails()
+	const [orderDetails] = useOrderDetails();
+
+	const pizzaArray = Array.from(orderDetails.pizzas.entries());
+	console.log(pizzaArray);
+	const listPizzas = pizzaArray.map(([key, value]) => {
+		return (
+			<li key={key}>
+				{value} {key}
+			</li>
+		);
+	});
+
 	return <div>Order Summary</div>;
 };
 
