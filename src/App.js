31bc516e3f5c1fe53c291f0c2af09541926fc775
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-22 18:47:36 +0000

    remove router

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import './App.css';
 
-import Navigation from './components/Navbar';
 import OrderEntry from './pages/entry/OrderEntry';
 import OrderSummary from './pages/summary/OrderSummary';
 import OrderConfirmation from './pages/confirmation/OrderConfirmation';
@@ -30,23 +29,11 @@ function App() {
 		default:
 	}
 	return (
-		<Router>
-			<Navigation />
-
-			<Container className='App'>
-				<Row style={{ marginTop: '20px' }}>
-					<Switch>
-						<OrderDetailsProvider>
-							<Route exact path='/' component={OrderEntry} />
-							<Route
-								path='/order-summary'
-								component={OrderSummary}
-							/>
-						</OrderDetailsProvider>
-					</Switch>
-				</Row>
-			</Container>
-		</Router>
+		<Container className='App'>
+			<Row style={{ marginTop: '20px' }}>
+				<OrderDetailsProvider></OrderDetailsProvider>
+			</Row>
+		</Container>
 	);
 }
 
