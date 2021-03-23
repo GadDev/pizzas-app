073afb8ae1f91f0fef4d21231f35404c69baed0d
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-23 23:47:03 +0000

    /expect input to be invalid with floating integer

--- src/pages/entry/tests/PizzaOptions.test.jsx
+++ src/pages/entry/tests/PizzaOptions.test.jsx
@@ -5,9 +5,14 @@ import PizzaOption from '../PizzaOption';
 test('indicate if pizza count is not integer or out of range', async () => {
 	render(<PizzaOption name='' image='' updateItemCount={jest.fn()} />);
 
-	//epxect input to be invalid with negative integer
+	//expect input to be invalid with negative integer
 	const optionInput = screen.getByRole('spinButton');
 	userEvent.clear(optionInput);
 	userEvent.type(optionInput, '-1');
+	expect(optionInput).toHaveClass('is-invalid');
+
+	//expect input to be invalid with floating integer
+	userEvent.clear(optionInput);
+	userEvent.type(optionInput, '2.5');
 	expect(optionInput).toHaveClass('is-invalid');
 });
