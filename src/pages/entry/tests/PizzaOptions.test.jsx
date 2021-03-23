a15deccf1ab78d8630e71c5792bd78a8b0d4c819
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-23 23:43:13 +0000

    find spinbutton

--- src/pages/entry/tests/PizzaOptions.test.jsx
+++ src/pages/entry/tests/PizzaOptions.test.jsx
@@ -4,4 +4,8 @@ import PizzaOption from '../PizzaOption';
 
 test('indicate if pizza count is not integer or out of range', async () => {
 	render(<PizzaOption name='' image='' updateItemCount={jest.fn()} />);
+
+	//epxect input to be invalid with negative integer
+	const pizzaAmerican = screen.getByRole('spinButton')
+	
 });
