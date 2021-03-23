dff5108d821c6bdfdad6bceb75672fe5b0a7de1a
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-23 23:45:49 +0000

    expect invalid class

--- src/pages/entry/tests/PizzaOptions.test.jsx
+++ src/pages/entry/tests/PizzaOptions.test.jsx
@@ -6,7 +6,8 @@ test('indicate if pizza count is not integer or out of range', async () => {
 	render(<PizzaOption name='' image='' updateItemCount={jest.fn()} />);
 
 	//epxect input to be invalid with negative integer
-	const optionInput = screen.getByRole('spinButton')
+	const optionInput = screen.getByRole('spinButton');
 	userEvent.clear(optionInput);
 	userEvent.type(optionInput, '-1');
+	expect(optionInput).toHaveClass('is-invalid');
 });
