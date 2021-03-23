40fc50ae0b55a1aa5ffef86677433d4ebeb14c04
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-23 23:39:54 +0000

    render PizaaOption render

--- src/pages/entry/tests/PizzaOptions.test.jsx
+++ src/pages/entry/tests/PizzaOptions.test.jsx
@@ -1,7 +1,7 @@
-import { render, screen } from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import PizzaOption from '../PizzaOption';
 
-test('indicate if pizza count is not integer or out of range', () => {
-
-})
+test('indicate if pizza count is not integer or out of range', async () => {
+	render(<PizzaOption name='' image='' updateItemCount={jest.fn()} />);
+});
