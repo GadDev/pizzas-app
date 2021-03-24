cc225e73b1e8dad333628a2af897e981b792d5f7
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-24 22:35:36 +0000

    import OrderConfirmation component

--- src/pages/confirmation/tests/OrderConfirmations.test.jsx
+++ src/pages/confirmation/tests/OrderConfirmations.test.jsx
@@ -4,3 +4,5 @@ import {
 } from '../../../test-utils/testing-library-utils';
 import { server } from '../../../mocks/server'
 import { rest } from "msw"
+
+import { OrderConfirmation } from '../OrderConfirmation';
