bdda9524a722637e699bbb96c4a15ff44282849c
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-24 22:39:04 +0000

    override default msw response with error response

--- src/pages/confirmation/tests/OrderConfirmations.test.jsx
+++ src/pages/confirmation/tests/OrderConfirmations.test.jsx
@@ -8,5 +8,10 @@ import { rest } from "msw"
 import { OrderConfirmation } from '../OrderConfirmation';
 
 test('error response server for submit order POST error request', () => {
-
+	//override default msw response with error response
+	server.resetHandlers([
+		rest.post('http://localhost:3030/order', (req,res,ctx) => {
+			res(ctx.status(500))
+		})
+	])
 })
