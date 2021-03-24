a5c8239641541ef192022a55d17e4748ce8c3b25
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-24 22:37:06 +0000

    test ---> error response server for submit order POST error request

--- src/pages/confirmation/tests/OrderConfirmations.test.jsx
+++ src/pages/confirmation/tests/OrderConfirmations.test.jsx
@@ -6,3 +6,7 @@ import { server } from '../../../mocks/server'
 import { rest } from "msw"
 
 import { OrderConfirmation } from '../OrderConfirmation';
+
+test('error response server for submit order POST error request', () => {
+
+})
