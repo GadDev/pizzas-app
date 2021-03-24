4bdd81678608cb7b9f3a19732cda69c6b66c54ec
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-24 22:42:31 +0000

    wip

--- src/pages/confirmation/tests/OrderConfirmations.test.jsx
+++ src/pages/confirmation/tests/OrderConfirmations.test.jsx
@@ -2,15 +2,16 @@ import { render, screen } from '../../../test-utils/testing-library-utils';
 import { server } from '../../../mocks/server';
 import { rest } from 'msw';
 
-import { OrderConfirmation } from '../OrderConfirmation';
+import OrderConfirmation from '../OrderConfirmation';
 
-test('error response server for submit order POST error request', () => {
+test('error response server for submit order POST error request', async () => {
 	//override default msw response with error response
-	server.resetHandlers([
+	server.resetHandlers(
 		rest.post('http://localhost:3030/order', (req, res, ctx) => {
 			res(ctx.status(500));
-		}),
-	]);
+		})
+	);
 
 	render(<OrderConfirmation setPhase={() => jest.fn()} />);
+	screen.debug();
 });
